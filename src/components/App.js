c446992875d2753935aa444d5e2abd4f96893705
PaulaPSoto <paezsotopaula@gmail.com>
2021-02-24 09:05:44 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import PokemonList from './PokemonList';
+import data from '../data/data.json';
+
+
---
+++ src/components/PokemonList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+import '../stylesheets/PokemonList.css'
+
+
+
